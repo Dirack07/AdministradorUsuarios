c495a6741f7175ff106b55b61ebb4453f29245ba
dirac <david161189@gmail.com>
2016-01-16 15:57:46 -0600

    new: dev: Agregar connector JDBC para MySQL

--- src/com/rdbc/conexion/ConexionBD.java
+++ src/com/rdbc/conexion/ConexionBD.java
@@ -1,60 +1,60 @@
 package com.rdbc.conexion;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
 public class ConexionBD {
-
+	
+	
 	@SuppressWarnings("finally")
-	public static Connection getConnection() {
-		Connection conexion = null;
-
+	public static Connection getConnection()
+    {
+        Connection conexion=null;
+      
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            String servidor = "jdbc:mysql://localhost/bdprueba";
+            String usuarioDB="root";
+            String passwordDB="aspire00";
+            conexion= DriverManager.getConnection(servidor,usuarioDB,passwordDB);
+        }
+        catch(ClassNotFoundException ex)
+        {
+            JOptionPane.showMessageDialog(null, ex, "Error1 en la Conexión con la BD "+ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion=null;
+        }
+        catch(SQLException ex)
+        {
+            JOptionPane.showMessageDialog(null, ex, "Error2 en la Conexión con la BD "+ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion=null;
+        }
+        catch(Exception ex)
+        {
+            JOptionPane.showMessageDialog(null, ex, "Error3 en la Conexión con la BD "+ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion=null;
+        }
+        finally
+        {
+            return conexion;
+        }
+    
+}
+	
+	
+	public static void closeConnetion(){
 		try {
-			Class driverClass = Class.forName("com.mysql.jdbc.Driver");
-			Driver driver = (Driver)driverClass.newInstance();
-			DriverManager.registerDriver(driver);
-			
-			String servidor = "jdbc:mysql://127.0.0.1:3306/bdprueba";
-			String usuarioDB = "root";
-			String passwordDB = "aspire00";
-			
-			conexion = DriverManager.getConnection(servidor, usuarioDB,
-					passwordDB);
-		} catch (ClassNotFoundException ex) {
-			JOptionPane.showMessageDialog(null, ex,
-					"Error1 en la Conexión con la BD " + ex.getMessage(),
-					JOptionPane.ERROR_MESSAGE);
-			conexion = null;
-		} catch (SQLException ex) {
-			JOptionPane.showMessageDialog(null, ex,
-					"Error2 en la Conexión con la BD " + ex.getMessage(),
-					JOptionPane.ERROR_MESSAGE);
-			conexion = null;
-		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(null, ex,
-					"Error3 en la Conexión con la BD " + ex.getMessage(),
-					JOptionPane.ERROR_MESSAGE);
-			conexion = null;
-		} finally {
-			return conexion;
-	  }
-
-	}
-
-	public static void closeConnetion() {
-		try {
-			if (getConnection() != null) {
-				getConnection().close();
+			if(getConnection() != null){
+				getConnection().close();	
 				System.out.println("Conexion de la base de datos cerrada");
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 	}
 }
